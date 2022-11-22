d2135e02676c62a8b2b9b3e7da567a2fd009db71
Martin Dyer <martin.dyer@sheffield.ac.uk>
2022-11-22 17:01:10 +0000

    Tidy handler

--- gtecs/alert/handler.py
+++ gtecs/alert/handler.py
@@ -3,8 +3,7 @@
 import logging
 
 from . import database as db
-from .slack import send_slack_msg, send_event_report, send_strategy_report, send_database_report
-from .events import Event
+from .slack import send_database_report, send_event_report, send_slack_msg, send_strategy_report
 
 
 def event_handler(event, send_messages=False, log=None):
@@ -18,6 +17,9 @@ def event_handler(event, send_messages=False, log=None):
     send_messages : bool, optional
         If True, send Slack messages.
         Default is False.
+    log : logging.Logger, optional
+        If given, direct log messages to this logger.
+        If None, a new logger is created.
 
     Returns
     -------
@@ -29,19 +31,15 @@ def event_handler(event, send_messages=False, log=None):
     # Create a logger if one isn't given
     if log is None:
         logging.basicConfig(level=logging.INFO)
-        log = logging.getLogger('goto-alert')
+        log = logging.getLogger('event_handler')
         log.setLevel(level=logging.DEBUG)
-
-    # Log IVORN
     log.info('Handling Event {}'.format(event.ivorn))
 
-    # Check if it's an event we want to process, otherwise return here
+    # 1) Check if it's an event we want to process, otherwise return here
     if not event.interesting:
         log.warning('Ignoring uninteresting event (type={}, role={})'.format(event.type,
                                                                              event.role))
         return False
-
-    # It passed the checks: it's an interesting event!
     log.info('Processing interesting {} Event {}'.format(event.type, event.name))
 
     # Send initial Slack report
@@ -51,12 +49,10 @@ def event_handler(event, send_messages=False, log=None):
         send_slack_msg(msg)
         log.debug('Slack report sent')
 
-    # Fetch the event skymap
-    if hasattr(event, 'get_skymap'):
-        # Not all "interesting" events will have a skymap (e.g. retractions)
-        log.debug('Fetching event skymap')
-        event.get_skymap()
-        log.debug('Skymap created')
+    # 2) Fetch the event skymap
+    log.debug('Fetching event skymap')
+    event.get_skymap()
+    log.debug('Skymap created')
 
     # Send Slack event report
     if send_messages:
@@ -68,25 +64,23 @@ def event_handler(event, send_messages=False, log=None):
             log.error('Error sending Slack report')
             log.debug(err.__class__.__name__, exc_info=True)
 
-    # If the event was a retraction there's no strategy
-    if not event.type == 'GW_RETRACTION':
-        # Get the observing strategy for this event (stored on the event as event.strategy)
-        # NB we can only do this after getting the skymap, because GW events need the distance.
-        log.debug('Fetching event strategy')
-        event.get_strategy()
-        log.debug('Using strategy {}'.format(event.strategy['strategy']))
+    # 3) Get the observing strategy for this event (stored on the event as event.strategy)
+    #    NB we can only do this after getting the skymap, because GW events need the distance.
+    log.debug('Fetching event strategy')
+    event.get_strategy()
+    log.debug('Using strategy {}'.format(event.strategy['strategy']))
 
-        # Send Slack strategy report
-        if send_messages:
-            log.debug('Sending Slack strategy report')
-            try:
-                send_strategy_report(event)
-                log.debug('Slack report sent')
-            except Exception as err:
-                log.error('Error sending Slack report')
-                log.debug(err.__class__.__name__, exc_info=True)
+    # Send Slack strategy report
+    if send_messages:
+        log.debug('Sending Slack strategy report')
+        try:
+            send_strategy_report(event)
+            log.debug('Slack report sent')
+        except Exception as err:
+            log.error('Error sending Slack report')
+            log.debug(err.__class__.__name__, exc_info=True)
 
-    # Add the event into the GOTO observation DB
+    # 4) Add the event into the GOTO observation DB
     log.info('Inserting event {} into GOTO database'.format(event.name))
     try:
         # First we need to see if there's a previous instance of the same event already in the db
@@ -122,17 +116,6 @@ def event_handler(event, send_messages=False, log=None):
 
         raise
 
+    # 5) Done
     log.info('Event {} processed'.format(event.name))
     return True
-
-
-def payload_handler(payload, send_messages=False):
-    """Handle a VOEvent payload.
-
-    Returns the Event if it is interesting, or None if it's been rejected.
-    """
-    # Create event from the payload
-    event = Event.from_payload(payload)
-
-    # Run the event handler
-    event_handler(event, send_messages)
